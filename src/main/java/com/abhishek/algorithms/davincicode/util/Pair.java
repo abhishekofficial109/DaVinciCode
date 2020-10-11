548bff50bbd86b3628ba4f48ff09f715b2c64c48
Abhishek Choudhury <abhishekofficial109@gmail.com>
2020-10-12 02:44:17 +0530

    Added a sorting based problem Merge Overlapping Intervals

---
+++ src/main/java/com/abhishek/algorithms/davincicode/util/Pair.java
@@ -0,0 +1,32 @@
+package com.abhishek.algorithms.davincicode.util;
+
+public class Pair<K, V> {
+	private K key;
+	private V value;
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+	public void setValue(V value) {
+		this.value = value;
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public V getValue() {
+		return value;
+	}
+
+	public Pair(K key, V value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	@Override
+	public String toString() {
+		return "Pair{" + "key=" + key + ", value=" + value + '}';
+	}
+}
